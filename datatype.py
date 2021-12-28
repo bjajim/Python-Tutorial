ca55e2f67b6006bb7bb71506172f3f05d1074d1d
Bolaji Ajimotokan <bjajim15@gmail.com>
2021-12-28 16:31:15 -0600

    class on datatype

---
+++ datatype.py
@@ -0,0 +1,46 @@
+"""
+this file is a discussion 
+about inbuilt data types
+
+"""
+
+
+
+# this is an example of text
+x = "hello world"
+print(x)
+print(type(x))
+
+y = '12345'
+print(y)
+print(type(y))
+
+
+#examples of numeric data type
+#example of integers
+
+z = 3000
+print(z)
+print(type(z))
+
+#example of a float
+
+w = 1.2
+print(w)
+print(type(w))
+
+r = True
+e = False
+
+print(r)
+print(type(r))
+
+print(e)
+print(type(e))
+
+
+#example1
+t = 20
+s = 40
+# find 2t + s
+print(2*t +s)
